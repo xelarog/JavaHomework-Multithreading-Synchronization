71fe1e5d587dc989ed3c76e2f13125a7812a02e3
Александр Горшков <stollprog@mail.ru>
2022-07-01 21:54:59 +0300

    init task2 v1.0

--- CarShowroom/src/ru/netology/Seller.java
+++ CarShowroom/src/ru/netology/Seller.java
@@ -1,21 +1,28 @@
 package ru.netology;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Seller {
     private final int SELL_CAR_TIME = 1000;
     private final int PRODUCTION_TIME = 3000;
 
-    private CarShowroom carShowroom;
+    private final ReentrantLock lock = new ReentrantLock(true);
+    private final Condition carCondition = lock.newCondition();
+
+    private final CarShowroom carShowroom;
 
     public Seller(CarShowroom carShowroom) {
         this.carShowroom = carShowroom;
     }
 
-    public synchronized Car sellCar() {
+    public Car sellCar() {
         try {
+            lock.lock();
             System.out.println(Thread.currentThread().getName() + " зашёл в автосалон");
             while (carShowroom.getCars().size() == 0) {
                 System.out.println("Машин нет");
-                wait();
+                carCondition.await();
             }
             Thread.sleep(SELL_CAR_TIME);
             System.out.println(Thread.currentThread().getName() + " уехал на новеньком авто");
@@ -23,18 +30,23 @@ public class Seller {
             System.out.println("машин продано - " + carShowroom.getCarSoldCount());
             return carShowroom.getCars().remove(0);
         } catch (InterruptedException e) {
+        } finally {
+            lock.unlock();
         }
         return null;
     }
 
-    public synchronized void recieveCar() {
+    public void recieveCar() {
         try {
+            lock.lock();
             Thread.sleep(PRODUCTION_TIME);
             System.out.println("Прозводитель выпустил 1 авто");
             carShowroom.getCars().add(new Car());
-            notify();
+            carCondition.signal();
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            lock.unlock();
         }
     }
 }
